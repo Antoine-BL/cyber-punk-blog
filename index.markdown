aa9dea6dcde9b9ce12a536448e253848be917fe1
Antoine Brassard Lahey <antoine.brassard@gmail.com>
2021-12-01 16:52:08 -0500

    Moar glitchy mwahaha

--- index.markdown
+++ index.markdown
@@ -5,15 +5,15 @@ layout: home
 
 <div class="articles-inner">
 {% for post in site.posts %}
-    <div class="post-card" style="cursor: pointer;" onclick="window.location='{{ post.url | relative_url }}';">
+    <div class="post-card text-glitch text-glitch-mouseover" style="cursor: pointer;" onclick="window.location='{{ post.url | relative_url }}';">
         <div class="article-title">
-            <div class="article-title-small">{{ post.title-small }}</div>
-            <div class="article-title-big">{{ post.title-big }}</div>
+            <div class="article-title-small glitch-target">{{ post.title-small }}</div>
+            <div class="article-title-big glitch-target">{{ post.title-big }}</div>
         </div>
         <div class="article-footer">
-            <div>Article</div>
-            <div>{{post.date | date: "%e %B %Y"}}</div>
-            <div class="tag">#{{post.type}}</div>
+            <div class="glitch-target">Article</div>
+            <div class="glitch-target">{{post.date | date: "%e %B %Y"}}</div>
+            <div class="tag glitch-target">#{{post.type}}</div>
         </div>
     </div>
 {% endfor %}
--- js/effects.js
+++ js/effects.js
@@ -4,7 +4,7 @@ $(document).ready(() => {
     $(document.body).addClass("loaded")
 
     applyNeon();
-    // applyTextGlitch();
+    applyTextGlitch();
 
     function applyNeon() {
         const targets = window.document.getElementsByClassName('neon')
@@ -35,29 +35,57 @@ $(document).ready(() => {
         const targets = window.document.getElementsByClassName('text-glitch');
         const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#%^&(){}:';
 
-        const textGlitch = target => {
-            originalText = target.innerText
-            for (let i = 0 ; i < 3; i++) {
-                let text = "";
-                for (let i = 0; i < originalText.length; i++) {
-                    letter = characters.charAt(Math.floor(Math.random() * characters.length))
-                    text += letter;
-                }
-                setTimeout(() => target.innerText = text, 10 * (i + 1));
-            }
-            setTimeout(() => target.innerText = originalText, 40);
+        const textGlitch = (target, originalText) => {
+            const delay = 50;
+            const nbIter = 4;
             
-            for (let child of target.children){
-                textGlitch(child)
+            if (target.children.length > 0) {
+                for (let child of target.children){
+                    textGlitch(child)
+                }
+            } else {
+                for (let i = 0 ; i < nbIter; i++) {
+                    let text = "";
+                    for (let i = 0; i < originalText.length; i++) {
+                        if (originalText.charAt(i) == ' '){
+                            letter = " ";
+                        } else {
+                            letter = characters.charAt(Math.floor(Math.random() * characters.length))
+                        }
+                        text += letter;
+                    }
+                    setTimeout(() => target.innerText = text, delay * i);
+                }
+                setTimeout(() => target.innerText = originalText, nbIter * delay);
             }
         }
 
         for (let i = 0; i < targets.length; i++) {
             const target = targets[i];
-            textGlitch(target);
+            const subtargets = [];
+            
+            subtargetElements = target.getElementsByClassName('glitch-target');
+            for (const subtarget of subtargetElements) {
+                subtargets.push([subtarget, subtarget.innerText]);
+            }
+
             if (target.classList.contains('text-glitch-mouseover')) {
-                target.addEventListener('mouseenter', () => textGlitch(target))
-                target.addEventListener('mouseleave', () => textGlitch(target))
+                target.addEventListener('mouseenter', () => {
+                    for (const sub of subtargets) {
+                        const el = sub[0];
+                        const ogText = sub[1];
+                        textGlitch(el, ogText);
+
+                    }
+                })
+                target.addEventListener('mouseleave', () => {
+                    for (const sub of subtargets) {
+                        const el = sub[0];
+                        const ogText = sub[1];
+                        textGlitch(el, ogText);
+
+                    }
+                })
             }
         }
     }
